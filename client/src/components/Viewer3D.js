8f24b9757d1a4aeb424d0f2da8be4b35b096d6a2
Nam-Anh1806 <namanhpham1806@gmail.com>
2026-03-01 20:57:44 +0700

    Create Viewer3D.js for Three.js 3D visualization

---
+++ client/src/components/Viewer3D.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+
+const Viewer3D = () => {
+  return (
+    <Canvas>
+      {/* Your 3D objects will go here */}
+    </Canvas>
+  );
+};
+
+export default Viewer3D;
